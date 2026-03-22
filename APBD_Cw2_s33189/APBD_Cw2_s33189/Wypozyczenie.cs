b455784ac5c433dedadbad46b12d0a6c92baad2d
s33189 <s33189@pjwstk.edu.pl>
2026-03-22 20:42:25 +0100

    Add Wypozyczenie

---
+++ APBD_Cw2_s33189/APBD_Cw2_s33189/Wypozyczenie.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_Cw2_s33189;
+
+public class Wypozyczenie
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public User User { get; set; }
+    public Equipment Equipment { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime? DataZwrotu { get; set; }
+    public DateTime TerminZwrotu { get; set; }
+    public bool CzyZwrocona { get; set; }
+}
