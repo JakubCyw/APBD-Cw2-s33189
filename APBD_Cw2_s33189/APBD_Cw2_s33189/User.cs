9ed6e28f765595199b7b67a32ae86287ae514cb4
s33189 <s33189@pjwstk.edu.pl>
2026-03-22 20:39:44 +0100

    Add users

---
+++ APBD_Cw2_s33189/APBD_Cw2_s33189/User.cs
@@ -0,0 +1,19 @@
+﻿namespace APBD_Cw2_s33189;
+
+public abstract class User
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public abstract int MaxWypozyczenia { get; }
+}
+
+public class Student : User
+{
+    public override int MaxWypozyczenia => 2;
+}
+
+public class Employee : User
+{
+    public override int MaxWypozyczenia => 5;
+}
